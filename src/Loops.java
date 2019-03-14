e97215f1178fb9a2cb7263912fe39f21c8cb90c5
yanhau <yanhau1000@hotmail.com>
2019-03-14 13:09:03 +0100

    First Hello program

--- src/Loops.java
+++ src/Loops.java
@@ -6,5 +6,10 @@ public class Loops {
         for (int i=0;i<5;i++) {
             System.out.println("i=" + i);
         }
+        for (int j=10;j>0;j--) {
+            if (j == 8) continue;
+            System.out.println("j=" + j);
+            if (j == 6) break;
+        }
      }
 }
