601fb5e913c3e4d7a57174ce7202827502b13a9a
yanhau <yanhau1000@hotmail.com>
2019-03-14 13:16:54 +0100

    First Hello program

--- src/HelloNew.java
+++ src/HelloNew.java
@@ -1,15 +1,14 @@
-public class Hello {
-    public static void main (String[] inputargs) {
-        System.out.println("Hello!!!");
+public class HelloNew {
+    public static void main(String[] args) {
+        System.out.println("HelloCode!!!");
 
         Integer a = 5;
         Integer b, c;
         b = a;
         c = b + 4;
-        c = new Integer( value: 5);
+        c = new Integer( 5);
         if (c == a) System.out.println ("c==a");
         if (c.equals(a)) System.out.println("c equals a");
-
         System.out.println("b=" + b);
         System.out.println("c=" + c);
 
@@ -18,12 +17,11 @@ public class Hello {
 
         String s3 = s1.toUpperCase();
         System.out.println(s3.charAt(1));
-        s1 = "Hello John";
+        s1 = "HelloCode John";
 
         int j = s1.indexOf("Jo");
         System.out.println(j);
 
         System.out.println(s1.substring(3,6));
-
     }
 }
