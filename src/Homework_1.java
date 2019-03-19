f1ba4add732304b21b827317259b95323271997b
yanhau <yanhau1000@hotmail.com>
2019-03-20 00:38:17 +0100

    Homework 1 Final

--- src/Homework_1.java
+++ src/Homework_1.java
@@ -2,11 +2,11 @@ public class Homework_1 {
     public static void main(String[] args) {
         String[] email = {"cp@gmail.com", "cpsummer@gmail.com", "nil"};
         for (String i : email) {
-            if (i.indexOf("@") > -1) & (i.indexOf(".com") > -1); {
-                System.out.println("The Email" + i + "is valid");
+            if (i.indexOf("@") > -1 && i.indexOf(".com") > -1) {
+                System.out.println("The Email " + i + " is valid");
             }
             else {
-                System.out.println("Email is Invalid");
+                System.out.println("The Email " + i + " is Invalid");
             }
 
             }
