644b6ea91bf1a4d44446cf89d07b5476f6ef53c4
yanhau <yanhau1000@hotmail.com>
2019-03-21 10:50:35 +0100

    Blocks

---
+++ src/Arrays.java
@@ -0,0 +1,15 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+        int[] intArr = new int[3];
+        intArr[0] = 567;
+        intArr[1] = 968;
+        intArr[2] = 675;
+
+        String[] strArr = new String[] {
+                "ABC",
+                "DEF",
+                "EFG"
+        };
+    }
+}
