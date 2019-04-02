cc2f0006c4a847dfd047aa10766b929092c9c590
yanhau <yanhau1000@hotmail.com>
2019-04-02 23:02:38 +0200

    Homework 3

--- src/Homework_3.java
+++ src/Homework_3.java
@@ -4,35 +4,14 @@ import java.util.Scanner;
 
 public class Homework_3 {
     public static void main(String[] args) throws IOException {
-        String path = "C:/Users/Yan Hau/Dropbox/Computer Programming/TTT.csv";
-        File file = new File(path);
-
-        String cwd = System.getProperty("user.dir");
-
-        String temp = System.getenv("TEMP");
-        System.out.println(temp);
-        System.out.println(System.getProperty("java.io.tmpdir"));
-        System.out.println(cwd);
+        String TTT = "C:/Users/Yan Hau/Dropbox/Computer Programming/TTT.csv";
+        File file = new File(TTT);
 
         Scanner fileScanner = new Scanner(file);
         while (fileScanner.hasNext()) {
-            String line = fileScanner.nextLine();
-            String[] cells = line.split(",");
-            System.out.println(cells[1]);
-            System.out.println(line);
+            String game = fileScanner.nextLine();
+            System.out.println(game);
         }
-
-        System.out.println(file.length());
-
-        System.out.println(file.getAbsolutePath());
-        System.out.println(file.getCanonicalPath());
-        System.out.println(file.getParentFile());
-
-
-
-
-
-
     }
 }
 
