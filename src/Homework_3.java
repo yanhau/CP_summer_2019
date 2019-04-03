09f7f5ada8bfcf6221ba93dc72666d35cfd83194
yanhau <yanhau1000@hotmail.com>
2019-04-03 23:07:14 +0200

    Homework 3

--- src/Homework_3.java
+++ src/Homework_3.java
@@ -2,12 +2,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-import static java.lang.Integer.parseInt;
+
 
 public class Homework_3 {
+    public static int[][] arr = new int[3][3];
+
+
     public static void main(String[] args) throws IOException {
-    int[][] board = new int[3][3];
-    int row = 0;
+    int[][] arr = new int[3][3];
     String path = "C:/Users/Yan Hau/Dropbox/Computer Programming/TTT.csv";
     File file = new File(path);
 
@@ -15,9 +17,43 @@ public class Homework_3 {
     while (fileScanner.hasNext()) {
         String line = fileScanner.nextLine();
         String[] cells = line.split(",");
-        board[0][0] = parseInt(cells[0]);
+        for (int i=0; i<3 ; i++) {
+            for (int j=0; j<3; j++) {
+                arr[j][i] = Integer.parseInt(cells[j]);
+            }
+        }
+        System.out.println(arr);
         System.out.println(line);
     }
+    if(// checking for horizontal victory
+            arr[0][0]+arr[0][1]+arr[0][2] ||
+            arr[1][0]+arr[1][1]+arr[1][2] ||
+            arr[2][0]+arr[2][1]+arr[2][2] ||
+       // checking for vertical victory
+            arr[0][0]+arr[1][0]+arr[2][0] ||
+            arr[0][1]+arr[1][1]+arr[2][1] ||
+            arr[0][2]+arr[1][2]+arr[2][2] ||
+       // checking for diagonal victory
+            arr[0][0]+arr[1][1]+arr[2][2] ||
+            arr[2][0]+arr[1][1]+arr[0][2] ||
+                    == 3) {
+            System.out.println("Player O has won");
+        }else if(
+                arr[0][0]+arr[0][1]+arr[0][2] ||
+                arr[1][0]+arr[1][1]+arr[1][2] ||
+                arr[2][0]+arr[2][1]+arr[2][2] ||
+
+                arr[0][0]+arr[1][0]+arr[2][0] ||
+                arr[0][1]+arr[1][1]+arr[2][1] ||
+                arr[0][2]+arr[1][2]+arr[2][2] ||
+
+                arr[0][0]+arr[1][1]+arr[2][2] ||
+                arr[2][0]+arr[1][1]+arr[0][2] ||
+                    == -3) {
+            System.out.println("Player X has won");
+        } else {
+            System.out.println("The game has ended in a draw");
+        }
     }
 }
 
