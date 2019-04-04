1e8ef40faf8d09131b1db4b7691ba02cf3546c33
yanhau <yanhau1000@hotmail.com>
2019-04-04 12:24:58 +0200

    Homework 3

--- src/pl/waw/sgh/FileSizeCalculator.java
+++ src/pl/waw/sgh/FileSizeCalculator.java
@@ -1,9 +1,33 @@
 package pl.waw.sgh;
 
+import java.io.File;
 import java.sql.SQLOutput;
 
 public class FileSizeCalculator {
-    String path = "C:/Users/Yan Hau/Dropbox/Computer Programming/FileSizeCalculator";
-    System.out.println(path.length())
+
+    public static long sumSize(File inFile) {
+        long sum = 0;
+        if(inFile.isDirectory()) {
+            for(File f: inFile.listFiles()) {
+                System.out.println(f.toString() + " " + f.length());
+                sum += f.length();
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        String INPUT_PATH = "C:/Users/Yan Hau/Dropbox/Computer Programming/FileSizeCalculator";
+        File mypath = new File(INPUT_PATH);
+
+        System.out.println(sumSize(mypath));
+// only reads files in this folder, but does not open any other subfolders
+        if (mypath.isDirectory()) {
+            for (File f : mypath.listFiles()) {
+                System.out.println(f.toString() + "" + f.length());
+            }
+        }
+
+    }
 
 }
