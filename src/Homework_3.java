c94bdad20a168422869579ff9926ac223f59a39e
yanhau <yanhau1000@hotmail.com>
2019-04-04 10:20:13 +0200

    Homework 3

--- src/Homework_3.java
+++ src/Homework_3.java
@@ -17,9 +17,9 @@ public class Homework_3 {
     while (fileScanner.hasNext()) {
         String line = fileScanner.nextLine();
         String[] cells = line.split(",");
-        arr[rownum][0] = cells[0];
-        arr[rownum][1] = cells[1];
-        arr[rownum][2] = cells[2];
+        arr[rownum][0] = Integer.parseInt(cells[0]);
+        arr[rownum][1] = Integer.parseInt(cells[1]);
+        arr[rownum][2] = Integer.parseInt(cells[2]);
         rownum++;
         /*for (int i=0; i<3 ; i++) {
             for (int j=0; j<3; j++) {
@@ -27,27 +27,27 @@ public class Homework_3 {
             }
         System.out.println(arr[1]);
         if(// checking for horizontal victory
-                        (arr[0][0]+arr[0][1]+arr[0][2]) ||
-                        (arr[1][0]+arr[1][1]+arr[1][2]) ||
-                        (arr[2][0]+arr[2][1]+arr[2][2]) ||
+                        (arr[0][0]+arr[0][1]+arr[0][2]) ==3 ||
+                        (arr[1][0]+arr[1][1]+arr[1][2]) ==3 ||
+                        (arr[2][0]+arr[2][1]+arr[2][2]) ==3||
            // checking for vertical victory
-                        (arr[0][0]+arr[1][0]+arr[2][0]) ||
-                        (arr[0][1]+arr[1][1]+arr[2][1]) ||
-                        (arr[0][2]+arr[1][2]+arr[2][2]) ||
+                        (arr[0][0]+arr[1][0]+arr[2][0]) ==3||
+                        (arr[0][1]+arr[1][1]+arr[2][1]) ==3||
+                        (arr[0][2]+arr[1][2]+arr[2][2]) ==3||
            // checking for diagonal victory
-                        (arr[0][0]+arr[1][1]+arr[2][2]) ||
-                        (arr[2][0]+arr[1][1]+arr[0][2])  == 3) {
+                        (arr[0][0]+arr[1][1]+arr[2][2]) ==3||
+                        (arr[2][0]+arr[1][1]+arr[0][2]) ==3) {
             System.out.println("Player O has won");
         }else if(
-                        (arr[0][0]+arr[0][1]+arr[0][2]) ||
-                        (arr[1][0]+arr[1][1]+arr[1][2]) ||
-                        (arr[2][0]+arr[2][1]+arr[2][2]) ||
+                        (arr[0][0]+arr[0][1]+arr[0][2]) ==-3||
+                        (arr[1][0]+arr[1][1]+arr[1][2]) ==-3||
+                        (arr[2][0]+arr[2][1]+arr[2][2]) ==-3||
 
-                        (arr[0][0]+arr[1][0]+arr[2][0]) ||
-                        (arr[0][1]+arr[1][1]+arr[2][1]) ||
-                        (arr[0][2]+arr[1][2]+arr[2][2]) ||
+                        (arr[0][0]+arr[1][0]+arr[2][0]) ==-3||
+                        (arr[0][1]+arr[1][1]+arr[2][1]) ==-3||
+                        (arr[0][2]+arr[1][2]+arr[2][2]) ==-3||
 
-                        (arr[0][0]+arr[1][1]+arr[2][2]) ||
+                        (arr[0][0]+arr[1][1]+arr[2][2]) ==-3||
                         (arr[2][0]+arr[1][1]+arr[0][2])  == -3) {
             System.out.println("Player X has won");
         } else {
