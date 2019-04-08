eb13f4a47a5f090887d43c766497bc3ffd5f5f28
yanhau <yanhau1000@hotmail.com>
2019-04-09 00:13:52 +0200

    File Writing

---
+++ src/Homework_4.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Homework_4 {
+
+    public static int rownum = 0;
+
+
+    public static void main(String[] args) throws IOException {
+        String path = "C:/Users/Yan Hau/Dropbox/Computer Programming/TTT.csv";
+        File file = new File(path);
+
+        Scanner fileScanner = new Scanner(file);
+        while (fileScanner.hasNext()) {
+            String line = fileScanner.nextLine();
+            String[] cells = line.split(",");
+            arr[rownum][0] = Integer.parseInt(cells[0]);
+            arr[rownum][1] = Integer.parseInt(cells[1]);
+            arr[rownum][2] = Integer.parseInt(cells[2]);
+            rownum++;
+        }
+
+
+}
