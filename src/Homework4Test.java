b06f2b5795de869cfb17a500ab4bd02b989bc168
yanhau <yanhau1000@hotmail.com>
2019-04-11 11:03:10 +0200

    File Writing

--- src/Homework4Test.java
+++ src/Homework4Test.java
@@ -1,24 +1,55 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
+import static java.lang.Double.parseDouble;
+
 public class Homework4Test {
-    public static int[][] stocks = new int[63][4];
+    public static Double[] newColumn = new Double[63];
     public static int rownum = 0;
+    public static int counter = 0;
+
+    public static void main(String path, String fileName) throws IOException {
+        String path1 = "C:/Users/Yan Hau/Dropbox/Computer Programming/Homework4.csv";
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        final String lineSep = System.getProperty("line.separator");
+        File file = new File(path1);
+        Scanner inputStream = new Scanner(file);
+        inputStream.next();
+        while (inputStream.hasNext()) {
+            String data = inputStream.next();
+            String[] values = data.split(",");
+            double openingPrice = parseDouble(values[1]);
+            double closingPrice = parseDouble(values[2]);
+/*            newColumn[rownum] = ((closingPrice - openingPrice) / openingPrice);
+            rownum++;*/
+            counter++;
+            System.out.println(counter);
+
+        /*try {
+            File file = new File(path, fileName);
+            File file2 = new File(path, fileName + ".1");
 
 
-    public static void main(String[] args) throws IOException {
-        String path = "C:/Users/Yan Hau/Dropbox/Computer Programming/Homework4.csv";
-        File file = new File(path);
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file2)));
+            String line = null;
+            int i = 0;
+            for (line = br.readLine(); line != null; line = br.readLine(), i++) {
 
-        Scanner fileScanner = new Scanner(file);
-        while (fileScanner.hasNext()) {
-            String line = fileScanner.nextLine();
-            String[] cells = line.split(",");
-            stocks[rownum][0] = Integer.parseInt(cells[0]);
-            stocks[rownum][1] = Integer.parseInt(cells[1]);
-            stocks[rownum][2] = Integer.parseInt(cells[2]);
-            rownum++;
+                String addedColumn = String.valueOf(data.get(i));
+                bw.write(line + addedColumn + lineSep);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            if (br != null)
+                br.close();
+            if (bw != null)
+                bw.close();
+        }*/
+
         }
     }
 }
--- src/Homework_4.java
+++ src/Homework_4.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -7,7 +8,9 @@ import static java.lang.Double.parseDouble;
 import static java.lang.String.valueOf;
 
 public class Homework_4 {
-    public static Double[] newColumn = new Double[62];
+    public static Double[] newColumn = new Double[63];
+    public static Double[] openingPriceList = new Double[63];
+    public static Double[] closingPriceList = new Double[63];
     public static int rownum = 0;
 
     public static void main(String[] args) throws IOException {
@@ -18,14 +21,45 @@ public class Homework_4 {
         while (inputStream.hasNext()) {
             String data = inputStream.next();
             String[] values = data.split(",");
-            double closingPrice = parseDouble(values[2]);
             double openingPrice = parseDouble(values[1]);
+            double closingPrice = parseDouble(values[2]);
+            openingPriceList[rownum] = openingPrice;
+            closingPriceList[rownum] = closingPrice;
             newColumn[rownum] = ((closingPrice - openingPrice) / openingPrice);
             rownum++;
-            System.out.println(newColumn);
-            }
 
         }
+
+        System.out.println(rownum);
+        System.out.println(Arrays.toString(newColumn));
+
+        String OUT_PATH = "C:/Users/Yan Hau/Dropbox/Computer Programming/Homework4A.csv";
+        File outFile = new File(OUT_PATH);
+        try {
+            BufferedWriter br = new BufferedWriter(new FileWriter(outFile, true));
+            StringBuilder sb = new StringBuilder();
+            sb.append("Opening Price");
+            sb.append(",");
+            sb.append("Closing Price");
+            sb.append(",");
+            sb.append("Difference in Price");
+            sb.append(",");
+            sb.append("\n");
+            for (int i=0;i<63;i++) {
+                sb.append(String.valueOf(openingPriceList[i]));
+                sb.append(",");
+                sb.append(String.valueOf(closingPriceList[i]));
+                sb.append(",");
+                sb.append(String.valueOf(newColumn[i]));
+                sb.append(",");
+                sb.append("\n");
+            }
+            br.write(sb.toString());
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+}
 
 
