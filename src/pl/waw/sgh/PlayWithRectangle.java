a9c4d123d30e27a456a81d5f2c7a1732f39bb504
yanhau <yanhau1000@hotmail.com>
2019-04-11 11:51:41 +0200

    File Writing

---
+++ src/pl/waw/sgh/PlayWithRectangle.java
@@ -0,0 +1,18 @@
+package pl.waw.sgh.obj;
+
+public class PlayWithRectangle {
+
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(4.9, 2.0);
+        //r1.setParams(3.0,4.0);
+        System.out.println("R1 surface: " + r1.calcSurface());
+
+        Rectangle r2 = new Rectangle();
+        //r1.setParams(3.0,4.0);
+        System.out.println("R2 surface: " + r2.calcSurface());
+
+        Rectangle r3 = new Rectangle(7.0);
+        //r1.setParams(3.0,4.0);
+        System.out.println("R3 surface: " + r3.calcSurface());
+    }
+}
---
+++ src/pl/waw/sgh/Rectangle.java
@@ -0,0 +1,33 @@
+package pl.waw.sgh.obj;
+
+public class Rectangle {
+
+    Double parA;
+    double parB;
+
+    public Rectangle() {
+        parA = 5.0;
+        parB = 6.0;
+    }
+
+    public Rectangle(double parA, double parB) {
+        setParams(parA, parB);
+    }
+
+    public Rectangle(double parA) {
+        this(parA, 0.0);
+        //super(); // Call constructor of super class (parent)
+        //this.parA = parA;
+    }
+
+    public void setParams(double parA, double b) {
+        this.parA = parA;
+        parB = b;
+    }
+
+    public double calcSurface() {
+        return parA * parB;
+    }
+
+
+}
