06a07476e33703478abf489c61fff8adfab3c9de
yanhau <yanhau1000@hotmail.com>
2019-04-24 18:52:59 +0200

    List of Shape + Comparator

--- src/Shapes/MapOfShapes.java
+++ src/Shapes/MapOfShapes.java
@@ -1,5 +1,26 @@
 package Shapes;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapOfShapes {
 
+    public static void main(String[] args) {
+        Map<String, Shape> myMapShape = new HashMap<>();
+        myMapShape.put("C1", new Circle(5));
+        myMapShape.put("R1", new Rectangle(7, 8));
+        myMapShape.put("C2", new Circle(6));
+
+        Circle c2  = (Circle)myMapShape.get("C2");
+        System.out.println("c2: " + c2);
+
+        System.out.println();
+        for (String k : myMapShape.keySet()) {
+            System.out.println(k + " is " + myMapShape.get(k));
+        }
+
+        Collection shapes = myMapShape.values();
+
+    }
 }
